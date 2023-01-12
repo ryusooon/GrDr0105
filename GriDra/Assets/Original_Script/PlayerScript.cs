d195f5321c80d531f0d38738aa1f80959d828822
BroomTeam <g1927053@st.t-kougei.ac.jp>
2023-01-12 19:33:26 +0900

    移動修正

--- GriDra/Assets/Original_Script/PlayerScript.cs
+++ GriDra/Assets/Original_Script/PlayerScript.cs
@@ -45,11 +45,11 @@ public class PlayerScript : MonoBehaviour
         Up_Force = new Vector3(0f, MoveSpeed1, 0f);
         Down_Force = new Vector3(0f, -MoveSpeed1, 0f);
 
-        Left_Force_X = new Vector3(-MoveSpeed2, 0f, 0f);
-        Right_Force_X = new Vector3(MoveSpeed2, 0f, 0f);
+        Left_Force_X = new Vector3(-MoveSpeed2, 0f, -MoveSpeed2);
+        Right_Force_X = new Vector3(MoveSpeed2, 0f, MoveSpeed2);
 
-        Left_Force_Z = new Vector3(0f, 0f, -MoveSpeed2);
-        Right_Force_Z = new Vector3(0f, 0f, MoveSpeed2);
+        Left_Force_Z = new Vector3(MoveSpeed2, 0f, -MoveSpeed2);
+        Right_Force_Z = new Vector3(-MoveSpeed2, 0f, MoveSpeed2);
 
         Center_x = MyObj.transform.position.x;
         Center_y = MyObj.transform.position.y;
@@ -62,23 +62,69 @@ public class PlayerScript : MonoBehaviour
         MyVec = DirectionObject.transform.up;
         //Debug.Log("MyVec:" + MyVec);
 
+        Debug.Log("MyVecX:" + MyVec.x);
+        Debug.Log("MyVecY:" + MyVec.y);
+        Debug.Log("MyVecZ:" + MyVec.z);
+
         Up_Force = new Vector3(0f, MoveSpeed1, 0f);
         Down_Force = new Vector3(0f, -MoveSpeed1, 0f);
 
-        Left_Force_X = new Vector3(-MoveSpeed2, 0f, 0f);
-        Right_Force_X = new Vector3(MoveSpeed2, 0f, 0f);
+        Left_Force_X = new Vector3(-MoveSpeed2, 0f, -MoveSpeed2);
+        Right_Force_X = new Vector3(MoveSpeed2, 0f, MoveSpeed2);
 
-        Left_Force_Z = new Vector3(0f, 0f, -MoveSpeed2);
-        Right_Force_Z = new Vector3(0f, 0f, MoveSpeed2);
+        Left_Force_Z = new Vector3(MoveSpeed2, 0f, -MoveSpeed2);
+        Right_Force_Z = new Vector3(-MoveSpeed2, 0f, MoveSpeed2);
     }
 
     void FixedUpdate()
     {
+        // 現在エリアの判別
+        //if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z <= 1.0 && MyVec.z >= 0.5))
+        if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z <= 1.0 && MyVec.z >= 0.5))
+        {
+            // 前向いてる時
+            FORWARD();
+        }
+        else if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z >= -1.0 && MyVec.z <= -0.5))
+        {
+            // 後ろ向いてる時
+            BACK();
+        }
+        else if ((MyVec.x < -0.5 && MyVec.x >= -1.0) && (MyVec.z < 0.5 && MyVec.z > -0.5))
+        {
+            // 左向いている時
+            LEFT();
+        }
+        else if ((MyVec.x > 0.5 && MyVec.x <= 1.0) && (MyVec.z < 0.5 && MyVec.z > -0.5))
+        {
+            // 右向いている時
+            RIGHT();
+        }
+        else
+        {
+            if (MyVec.z < -0.5)
+            {
+                Debug.Log("隙間エリア(後方)の左右移動");
+                BACK();
+            }
+            else if (MyVec.z > -0.5)
+            {
+                Debug.Log("隙間エリア(前方)の左右移動");
+                FORWARD();
+            }
+            else
+            {
+                Debug.Log("隙間エリア");
+                Debug.Log("隙間エリアのX" + MyVec.x);
+                Debug.Log("隙間エリアのZ" + MyVec.z);
+            }
+        }
+
 
         //上下移
-        if(Hit.Up)
+        if (Hit.Up)
         {
-            Up();
+            UP();
         }
         else
         {
@@ -87,7 +133,7 @@ public class PlayerScript : MonoBehaviour
 
         if (Hit.Down)
         {
-            Down();
+            DOWN();
         }
         else
         {
@@ -95,20 +141,89 @@ public class PlayerScript : MonoBehaviour
         }
 
 
-        //左右移動
-        if (Hit2.Right)
-        {
 
-            Right();
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
+    }
+
+    void UP()
+    {
+        MyRig.AddForce(Up_Force, ForceMode.Impulse);
+        Debug.Log("Up");
+    }
+
+    void DOWN()
+    {
+        MyRig.AddForce(Down_Force, ForceMode.Impulse);
+        Debug.Log("Down");
+    }
+
+    void FORWARD()
+    {
+
+        // 前向いている時の左右移動
 
         if (Hit2.Left)
         {
-            Left();
+            // 前向いてる時の左移動
+            MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
+            Debug.Log("FのL");
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+        if (Hit2.Right)
+        {
+            // 前向いてる時の右移動
+            MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
+            Debug.Log("FのR");
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+    }
+
+    void BACK()
+    {
+
+        // 後ろ向いている時の左右移動
+
+        if (Hit2.Left)
+        {
+            // 後ろ向いてる時の左移動
+            MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
+            Debug.Log("BのL");
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+        if (Hit2.Right)
+        {
+            // 後ろ向いてる時の右移動
+            MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
+            Debug.Log("BのR");
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+    }
+
+    void LEFT()
+    {
+
+        // 左向いている時の左右移動
+
+        if (Hit2.Left)
+        {
+            //左向いている時の左移動
+            MyRig.AddForce(Left_Force_Z, ForceMode.Impulse);
+            Debug.Log("LのL");
         }
         else
         {
@@ -116,73 +231,47 @@ public class PlayerScript : MonoBehaviour
         }
 
 
-    }
-
-    void Up()
-    {
-        MyRig.AddForce(Up_Force, ForceMode.Impulse);
-        //Debug.Log("Up");
-    }
-
-    void Down()
-    {
-        MyRig.AddForce(Down_Force, ForceMode.Impulse);
-       // Debug.Log("Down");
-    }
-
-    void Left()
-    {
-
-        // 左移動
-        if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z <= 1.0 && MyVec.z >= 0.5))
-        {
-            // 前向いてる時の左移動
-            MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
-        }
-        else if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z >= -1.0 && MyVec.z <= -0.5))
-        {
-            // 後ろ向いてる時の左移動
-            MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
-        }
-        else if ((MyVec.x <= -0.5 && MyVec.x >= -1.0) && (MyVec.z <= 0.5 && MyVec.z >= -0.5))
-        {
-            //左向いている時の左移動
-            MyRig.AddForce(Left_Force_Z, ForceMode.Impulse);
-        }
-        else if ((MyVec.x >= 0.5 && MyVec.x <= 1.0) && (MyVec.z <= 0.5 && MyVec.z >= -0.5))
-        {
-            // 右向いている時の右移動
-            MyRig.AddForce(Right_Force_Z, ForceMode.Impulse);
-        }
-
-        //MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
-        //Debug.Log("Left");
-
-    }
-
-    void Right()
-    {
-
-        // 右移動
-        if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z <= 1.0 && MyVec.z >= 0.5))
-        {
-            // 前向いてる時の右移動
-            MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
-        }
-        else if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z >= -1.0 && MyVec.z <= -0.5))
-        {
-            // 後ろ向いてる時の右移動
-            MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
-        }
-        else if ((MyVec.x <= -0.5 && MyVec.x >= -1.0) && (MyVec.z <= 0.5 && MyVec.z >= -0.5))
+        if (Hit2.Right)
         {
             // 左向いている時の右移動
             MyRig.AddForce(Right_Force_Z, ForceMode.Impulse);
+            Debug.Log("LのR");
         }
-        else if ((MyVec.x >= 0.5 && MyVec.x <= 1.0) && (MyVec.z <= 0.5 && MyVec.z >= -0.5))
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+        //MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
+        Debug.Log("Left");
+
+    }
+
+    void RIGHT()
+    {
+        // 右向いている時の左右移動
+
+        if (Hit2.Left)
+        {
+            // 右向いている時の左移動
+            MyRig.AddForce(Right_Force_Z, ForceMode.Impulse);
+            Debug.Log("RのL");
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+
+        if (Hit2.Right)
         {
             // 右向いている時の右移動
             MyRig.AddForce(Left_Force_Z, ForceMode.Impulse);
+            Debug.Log("RのR");
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
         }
 
         //MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
