b31cb9b77df8b778fbf0679cb0fb0de0e1bd4535
BroomTeam <g1927053@st.t-kougei.ac.jp>
2023-01-24 13:56:31 +0900

    左右移動を新しくした

--- GriDra/Assets/Original_Script/PlayerScript.cs
+++ GriDra/Assets/Original_Script/PlayerScript.cs
@@ -11,14 +11,6 @@ public class PlayerScript : MonoBehaviour
     Vector3 Up_Force;
     Vector3 Down_Force;
 
-    //前後を向いている場合に使うVec
-    Vector3 Left_Force_X;
-    Vector3 Right_Force_X;
-
-    //前後を向いている場合に使うVec
-    Vector3 Left_Force_Z;
-    Vector3 Right_Force_Z;
-
     private float Center_x;
     private float Center_y;
     private float Center_z;
@@ -37,8 +29,8 @@ public class PlayerScript : MonoBehaviour
 
     public string LookDirection;
 
-    //public float ForcePower = 0.25f;
-    //public float MaxVal = 1.5f;
+    [SerializeField] Transform Broom;
+    Vector3 Broom_Left, Broom_Right;
 
     // Start is called before the first frame update
 
@@ -47,68 +39,48 @@ public class PlayerScript : MonoBehaviour
         Up_Force = new Vector3(0f, MoveSpeed1, 0f);
         Down_Force = new Vector3(0f, -MoveSpeed1, 0f);
 
-        Left_Force_X = new Vector3(-MoveSpeed2, 0f, -MoveSpeed2);
-        Right_Force_X = new Vector3(MoveSpeed2, 0f, MoveSpeed2);
-
-        Left_Force_Z = new Vector3(MoveSpeed2, 0f, -MoveSpeed2);
-        Right_Force_Z = new Vector3(-MoveSpeed2, 0f, MoveSpeed2);
-
         Center_x = MyObj.transform.position.x;
         Center_y = MyObj.transform.position.y;
         Center_z = MyObj.transform.position.z;
 
-        LookDirection = "North";
+        Broom_Left = Broom.transform.right;
+        Broom_Right = -Broom.transform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
         MyVec = DirectionObject.transform.up;
-        //Debug.Log("MyVec:" + MyVec);
-
-        ForceCheck();
-
-        //Debug.Log("MyVecX:" + MyVec.x);
-        //Debug.Log("MyVecY:" + MyVec.y);
-        //Debug.Log("MyVecZ:" + MyVec.z);
 
         Up_Force = new Vector3(0f, MoveSpeed1, 0f);
         Down_Force = new Vector3(0f, -MoveSpeed1, 0f);
 
-        //Left_Force_X = new Vector3(-MoveSpeed2, 0f, -MoveSpeed2);
-        //Right_Force_X = new Vector3(MoveSpeed2, 0f, MoveSpeed2);
-
-        //Left_Force_Z = new Vector3(MoveSpeed2, 0f, -MoveSpeed2);
-        //Right_Force_Z = new Vector3(-MoveSpeed2, 0f, MoveSpeed2);
+        Broom_Left = Broom.transform.right;
+        Broom_Right = -Broom.transform.right;
     }
 
     void FixedUpdate()
     {
         // 現在エリアの判別
-        //if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z <= 1.0 && MyVec.z >= 0.5))
         if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z <= 1.0 && MyVec.z >= 0.5))
         {
             // 前向いてる時
             Debug.Log("方向：前");
-            FORWARD();
         }
         else if ((MyVec.x >= -0.5 && MyVec.x <= 0.5) && (MyVec.z >= -1.0 && MyVec.z <= -0.5))
         {
             // 後ろ向いてる時
             Debug.Log("方向：後ろ");
-            BACK();
         }
         else if ((MyVec.x < -0.5 && MyVec.x >= -1.0) && (MyVec.z < 0.5 && MyVec.z > -0.5))
         {
             // 左向いている時
             Debug.Log("方向：左");
-            LEFT();
         }
         else if ((MyVec.x > 0.5 && MyVec.x <= 1.0) && (MyVec.z < 0.5 && MyVec.z > -0.5))
         {
             // 右向いている時
             Debug.Log("方向：右");
-            RIGHT();
         }
         else
         {
@@ -116,13 +88,11 @@ public class PlayerScript : MonoBehaviour
             {
                 //Debug.Log("隙間エリア(後方)の左右移動");
                 Debug.Log("方向:隙間後ろ");
-                BACK();
             }
             else if (MyVec.z > -0.5)
             {
                 //Debug.Log("隙間エリア(前方)の左右移動");
                 //Debug.Log("方向:隙間前");
-                FORWARD();
             }
             else
             {
@@ -133,7 +103,7 @@ public class PlayerScript : MonoBehaviour
         }
 
 
-        //上下移
+        //上下移動
         if (Hit.Up)
         {
             UP();
@@ -146,6 +116,26 @@ public class PlayerScript : MonoBehaviour
         if (Hit.Down)
         {
             DOWN();
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+
+        // 左右移動
+        if (Hit2.Left)
+        {
+            LEFT();  
+        }
+        else
+        {
+            MyRig.velocity = Vector3.zero;
+        }
+
+        if (Hit2.Right)
+        {
+            RIGHT();
         }
         else
         {
@@ -166,165 +156,19 @@ public class PlayerScript : MonoBehaviour
         //Debug.Log("Down");
     }
 
-    void FORWARD()
-    {
-
-        // 前向いている時の左右移動
-
-        if (Hit2.Left)
-        {
-            // 前向いてる時の左移動
-            MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
-            //Debug.Log("FのL");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-        if (Hit2.Right)
-        {
-            // 前向いてる時の右移動
-            MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
-            Debug.Log("FのR");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-    }
-
-    void BACK()
-    {
-
-        // 後ろ向いている時の左右移動
-
-        if (Hit2.Left)
-        {
-            // 後ろ向いてる時の左移動
-            MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
-            Debug.Log("BのL");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-        if (Hit2.Right)
-        {
-            // 後ろ向いてる時の右移動
-            MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
-            Debug.Log("BのR");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-    }
-
     void LEFT()
     {
 
-        // 左向いている時の左右移動
-
-        if (Hit2.Left)
-        {
-            //左向いている時の左移動
-            MyRig.AddForce(Left_Force_Z, ForceMode.Impulse);
-            Debug.Log("LのL");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-
-        if (Hit2.Right)
-        {
-            // 左向いている時の右移動
-            MyRig.AddForce(Right_Force_Z, ForceMode.Impulse);
-            Debug.Log("LのR");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-        //MyRig.AddForce(Left_Force_X, ForceMode.Impulse);
-        Debug.Log("Left");
-
+        // 左移動
+        MyRig.AddForce(Broom_Left * MoveSpeed2, ForceMode.Impulse);
+        //Debug.Log("Left");
     }
 
     void RIGHT()
     {
-        // 右向いている時の左右移動
-
-        if (Hit2.Left)
-        {
-            // 右向いている時の左移動
-            MyRig.AddForce(Right_Force_Z, ForceMode.Impulse);
-            Debug.Log("RのL");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-
-        if (Hit2.Right)
-        {
-            // 右向いている時の右移動
-            MyRig.AddForce(Left_Force_Z, ForceMode.Impulse);
-            Debug.Log("RのR");
-        }
-        else
-        {
-            MyRig.velocity = Vector3.zero;
-        }
-
-        //MyRig.AddForce(Right_Force_X, ForceMode.Impulse);
-        Debug.Log("Right");
-
-    }
-
-    void ForceCheck()
-    {
-        // LookDirectionを元に向いている方角と、方角に合わせて掛けるべき力のパターンを判別
-
-        if (LookDirection == "North")
-        {
-            Left_Force_X = new Vector3(-MoveSpeed2, 0f, -MoveSpeed2);
-            Right_Force_X = new Vector3(MoveSpeed2, 0f, MoveSpeed2);
-
-            Left_Force_Z = new Vector3(MoveSpeed2, 0f, -MoveSpeed2);
-            Right_Force_Z = new Vector3(-MoveSpeed2, 0f, MoveSpeed2);
-        }
-        else if (LookDirection == "West")
-        {
-            Left_Force_X = new Vector3(MoveSpeed2, 0f, MoveSpeed2);
-            Right_Force_X = new Vector3(-MoveSpeed2, 0f, -MoveSpeed2);
-
-            Left_Force_Z = new Vector3(0f, 0f, MoveSpeed2);
-            Right_Force_Z = new Vector3(0f, 0f, -MoveSpeed2);
-        }
-        else if (LookDirection == "South")
-        {
-            Left_Force_X = new Vector3(MoveSpeed2, 0f, 0f);
-            Right_Force_X = new Vector3(-MoveSpeed2, 0f, 0f);
-
-            Left_Force_Z = new Vector3(0f, 0f, MoveSpeed2);
-            Right_Force_Z = new Vector3(0f, 0f, -MoveSpeed2);
-        }
-        else if (LookDirection == "East")
-        {
-            Left_Force_X = new Vector3(0f, 0f, MoveSpeed2);
-            Right_Force_X = new Vector3(0f, 0f, -MoveSpeed2);
-
-            Left_Force_Z = new Vector3(-MoveSpeed2, 0f, 0f);
-            Right_Force_Z = new Vector3(MoveSpeed2, 0f, 0f);
-        }
+        // 右移動
+        MyRig.AddForce(Broom_Right * MoveSpeed2, ForceMode.Impulse);
+        //Debug.Log("Right");
 
     }
 
